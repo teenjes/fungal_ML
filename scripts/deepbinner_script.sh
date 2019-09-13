4fde0728420d38a8c6436c66137cad3564b8d42e
Tavish Eenjes <teenjes@outlook.com>
2019-09-13 15:28:44 +1000

    Changes made to folder structure and base README

--- scripts/deepbinner_script.sh
+++ scripts/deepbinner_script.sh
@@ -4,10 +4,6 @@
 # It requires an input directory that contains directories with all reads from a single MinION flowcell - these subdirectories are examined one-by-one by the script
 # It requires an output directory as a location for the deposit of demultiplexed reads. This should be labelled with the flowcell run
 
-
-in_loc="$1"
-out_loc="$2"
-
 WHITE='\033[1;37m'
 RED='\033[0;31m'
 NC='\033[0m'
@@ -15,7 +11,7 @@ GREEN='\033[0;32m'
 
 echo -e "${GREEN}This script takes all folders within the input directory ${RED}"$1"${GREEN} and runs Deepbinner of these files to demultiplex the fast5 files within these folders and output them into barcode directories within the output directory ${RED}"$2"${WHITE}"
 
-"
+
 for d in "$1"*/ ; do
 	deepbinner realtime --in_dir "$d" --out_dir "$2" --native
-done"
+done
