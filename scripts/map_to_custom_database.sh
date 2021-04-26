fcfa19503b61700f5bef847d3d8d0ee6c8c53507
Tavish Eenjes <tavish.eenjes@anu.edu.au>
2021-04-27 09:43:59 +1000

    commit test

--- scripts/map_to_custom_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash                                                                                             
-WHITE='\033[1;37m'
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-
-for file in analysis/database_mapping/custom/*.fasta; do
-	        minimap2 -cx map-ont $1 $file > ${file%.fasta}.paf
-	done
