bb60ac08eff6767ce3955c0f10718faa94a1941f
teenjes <45346730+teenjes@users.noreply.github.com>
2021-12-16 12:42:04 +1100

    Update homology_filtering_command.sh

--- scripts/homology_filtering_commands.sh
+++ scripts/homology_filtering_commands.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+### Requires minimap2 and ONT reads
 
-echo -e "This script works through all folders within the input directory "$1" and uses the minimap2 program to map reads against a custom-curated database formed of 29 sequences for assessing frDNA homology"
+
+echo -e "This script takes an input fastq file "$1" and uses the minimap2 program to map reads against a custom-curated database ($3), saving to output directory "$2""
 
 # for files in the original unfiltered fastq, apply minimap2 for homology filtering
-for d in "$1"/Concatenated; do
-	for f in "$d"/*; do
-		echo "$f"
-		remove='analysis/Concatenated'
-		output=${f//$remove}
-		echo "$output"
-		minimap2 -x map-ont analysis/Python_Processing/homology/homology_genus.fasta "$f"/merged.fastq > analysis/Python_Processing"$output"/combined_test.paf
-done
-done
+
+# input = $1
+# output = $2
+# custom database is at $3
+
+
+
+minimap2 -x map-ont $3 $1 > "$2"/mapped.paf
